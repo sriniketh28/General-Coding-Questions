7a79e00601a836c95fe0ef20bf8340efae396343
Muppaneni Sriniketh <39892279+sriniketh28@users.noreply.github.com>
2021-05-30 11:27:25 +0530

    Create solution.py

---
+++ Arrays/Best-Time-to-Buy-and-Sell-Stock/solution.py
@@ -0,0 +1,10 @@
+def maxProfit(prices):
+	max_profit, min_price = 0, float('inf')
+	for price in prices:
+		min_price = min(min_price, price)
+		profit = price - min_price
+		max_profit = max(max_profit, profit)
+	return max_profit
+
+prices = [7,1,5,3,6,4]
+print(maxProfit(prices))
